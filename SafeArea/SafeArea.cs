407135ba3d152c4a8db2221aacf106b9b474894c
GameDevGo <gamedevgo.sb@gmail.com>
2026-06-23 14:27:11 +0530

    Safe area script added

---
+++ SafeArea/SafeArea.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class SafeArea : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Rect lastSafeArea;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+
+        if (safeArea != lastSafeArea)
+        {
+            lastSafeArea = safeArea;
+
+            Vector2 anchorMin = safeArea.position;
+            Vector2 anchorMax = safeArea.position + safeArea.size;
+
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+
+            rectTransform.anchorMin = anchorMin;
+            rectTransform.anchorMax = anchorMax;
+        }
+    }
+
+    void Update()
+    {
+        if (Screen.safeArea != lastSafeArea)
+        {
+            ApplySafeArea();
+        }
+    }
+}
